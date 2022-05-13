fccd68bfb9d14a880572c451840e577c255fc764
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-05-13 13:31:16 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[]
+            {
+                5,78,768,23,345,546,345
+            };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+                
+            }
+
+        }
+    }
+}
